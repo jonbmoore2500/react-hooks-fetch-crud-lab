d6f5b33b3dd04e189c9951397cc2ecdba1932f41
Jonathan <jonathanmoorevla@gmail.com>
2022-09-16 17:00:34 -0500

    Build QuestionForm, POST

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions, setQuestions] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then(r => r.json())
+    .then(questionsData => setQuestions(questionsData))
+  }, [])
+
+  function onNewQuestion(newQuestions) {
+    setQuestions([...questions, newQuestions])
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm onNewQuestion={onNewQuestion}/> : <QuestionList questions={questions}/>}
     </main>
   );
 }
