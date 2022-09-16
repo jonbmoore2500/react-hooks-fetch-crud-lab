f7db2fd534579845c9ee0ff0aa84a9b1a0960e40
Jonathan <jonathanmoorevla@gmail.com>
2022-09-16 17:29:05 -0500

    Handle delete button

--- src/components/App.js
+++ src/components/App.js
@@ -16,10 +16,21 @@ function App() {
     setQuestions([...questions, newQuestions])
   }
 
+  function onDelete(deleteId) {
+    fetch(`http://localhost:4000/questions/${deleteId}`, {
+    method: "DELETE"
+    })
+    .then(r => r.json())
+    .then(() => {
+      const newQuestions = questions.filter(question => question.id !== deleteId)
+      setQuestions(newQuestions)
+    })
+  }
+
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm onNewQuestion={onNewQuestion}/> : <QuestionList questions={questions}/>}
+      {page === "Form" ? <QuestionForm onNewQuestion={onNewQuestion}/> : <QuestionList questions={questions} onDelete={onDelete}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,16 +1,19 @@
 import React from "react";
 import QuestionItem from "./QuestionItem.js"
 
-function QuestionList({questions}) {
+function QuestionList({questions, onDelete}) {
   
-  
+  function onDeleteQL(deleteId) {
+    onDelete(deleteId)
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>
-        {questions.map((question) => (
-        <QuestionItem key={question.id} question={question}/>
-        ))}
+        {questions.map((question) => {
+        return <QuestionItem key={question.id} question={question} handleDelete={onDeleteQL}/>
+        })}
       </ul>
     </section>
   );
